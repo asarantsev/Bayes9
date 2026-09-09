95576c61f01aea2ddadbe8136ccfdc391b076f83
Andrey Sarantsev <50476640+asarantsev@users.noreply.github.com>
2026-09-09 15:05:17 -0700

    Update bayesSim9.py

--- bayesSim9.py
+++ bayesSim9.py
@@ -5,7 +5,7 @@
 # BAA bond rates R, 10-year Treasury rates L 
 # We use the valuation measure based on 1-year dividends
 # initialF = initial factor (F = V, H, L, R)
-# T is time horizon in years
+# T is time horizon in years 
 # returns five 2d arrays, each has rows which are time series simulations
 
 # geometric nominal returns 
@@ -34,18 +34,24 @@ def YJinv(y):
 
 NSIMS = 10000
 
+# coefficient estimates and Bayesian covariance matrix for US stock returns
+# order: 1/vol, -np.diff(rates)/vol, -measure[:-1]/vol, 1
 pointUS  = numpy.array([0.26369255, 0.05531972, 0.13032606, -0.01289155])
 covUS = (numpy.array([[ 1.20309794e-03, -8.83528968e-05, 7.70409213e-04, -1.17693913e-04],
  [-8.83528968e-05,  2.01281081e-04, -9.91713817e-05,  6.70198050e-06],
  [ 7.70409213e-04, -9.91713817e-05,  1.90810867e-03, -3.21309983e-05],
  [-1.17693913e-04,  6.70198050e-06, -3.21309983e-05,  1.49705561e-05]]))
 
+# coefficient estimates and Bayesian covariance matrix for developed stock returns
+# order: 1/vol, -np.diff(rates)/vol, -measure[:-1]/vol, 1
 pointDev = numpy.array([ 0.27787639,  0.03958753,  0.0798607,  -0.01722616])
 covDev = (numpy.array([[3.67447998e-03, -3.52301676e-04, 6.92192902e-04, -4.02309553e-04],
  [-3.52301676e-04, 3.87530356e-04, 3.94405888e-05, 3.54513032e-05],
  [ 6.92192902e-04, 3.94405888e-05, 5.81253354e-03, 5.78669736e-05],
  [-4.02309553e-04, 3.54513032e-05, 5.78669736e-05, 5.27845045e-05]]))
 
+# coefficient estimates and Bayesian covariance matrix for emerging stock returns
+# order: 1/vol, -np.diff(rates)/vol, -measure[:-1]/vol, 1
 pointEM = numpy.array([ 0.23986833, 0.09262668, -0.03340665, -0.01555279])
 covEM = (numpy.array([[ 0.01124397, -0.00177033,  0.00206245, -0.00116777],
  [-0.00177033,  0.00204362, -0.00078455,  0.00012877],
@@ -55,23 +61,34 @@ covEM = (numpy.array([[ 0.01124397, -0.00177033,  0.00206245, -0.00116777],
 pointBAA = numpy.array([-0.00165298,  0.06097906])
 covBAA =  numpy.array([[ 2.20325613e-07, -1.87489568e-07], [-1.87489568e-07,  1.34754388e-05]])
 
+# coefficient estimates and Bayesian covariance matrix for transformed log volatility
+# order: 1, nvol[:-1] (lag)
 pointNVol = numpy.array([0.3515266, 1-0.39913095])
 covNVol = numpy.array([[ 0.00527463, -0.00594903], [-0.00594903, 0.00676201]])
 
+# TBD
 pointRates = numpy.array([ 0.0707774, -0.04409479])
 covRates = numpy.array([[0.0026996, -0.00143464], [-0.00143464, 0.00079885]])
 
+# coefficient estimates and Bayesian covariance matrix for the new valuation measure
+# order: 1/vol, measure[:-1]/vol, 1
 pointMeasure = numpy.array([ 0.16985524, -0.17381195, -0.01291036])
 covMeasure = (numpy.array([[ 1.79226532e-03, -1.11890460e-03, -1.76641291e-04],
  [-1.11890460e-03,  2.86199447e-03,  4.43772314e-05],
  [-1.76641291e-04,  4.43772314e-05,  2.27011199e-05]]))
 
+# coefficient estimates and Bayesian covariance matrix for transformed log volatility
+# order: 1, lspreads[:-1] (lag)
 pointLSpreads = numpy.array([-0.19795661, -0.18730095])
 covLSpreads = numpy.array([[0.00495171, 0.00361859], [0.00361859, 0.00349211]])
 
+# coefficient estimates and Bayesian covariance matrix for zeros returns
+# versus the benchmark = (7 * long[34:-1] - 6 * long[35:]) * 0.01 
+# order: 1, benchmark
 pointZeros =  numpy.array([0.00354591, 1.00749553])
 covZeros = numpy.array([[ 7.37064950e-06, -4.55045562e-05], [-4.55045562e-05, 7.94059221e-04]])
 
+# general 9x9 covariance matrix for innovations
 covInnovations = (0.0001 * numpy.array(
 [[2.02636925, 0.83602418, -0.08710778, 0.21156024, -0.61493991, -0.1539649, 2.06356837, -9.89460925,  0.29285218],
 [0.836024184, 2.91381397,  2.18979084, -0.0140301215, -0.706718228, 0.0635922479, 0.903729142, -20.3853645, 0.165924589],
